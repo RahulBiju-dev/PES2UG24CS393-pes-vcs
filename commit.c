2a8410c28782e7d32c69e16f79973fa98dfe7a55
Rahul B <rnair5603@gmail.com>
2026-04-21 12:39:32 +0530

    feat(phase4): Scaffold commit_create and read parent hash

--- commit.c
+++ commit.c
@@ -194,8 +194,11 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Commit c;
+    memset(&c, 0, sizeof(Commit));
+    
+    if (head_read(&c.parent) == 0) {
+        c.has_parent = 1;
+    }
+    return 0;
 }
